eb1af6baea2f9e2eee7506075d2a6955d5f73d37
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2017-02-21 22:53:53 -0700

    Added structure for header/footer files using EJS templates

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var webServer = express();
 var path = require("path");
 var webpagePath = path.join(__dirname, "/public");
 var bodyParser = require("body-parser");
+var fileSystem = require("fs");
+var ejs = require("EJS");
 
 
 
@@ -12,10 +14,21 @@ webServer.use(bodyParser.json());
 webServer.use(bodyParser.urlencoded({ extended: true}));
 webServer.use(express.static(webpagePath));
 
+webServer.set('view engine', 'ejs');
+
 
 webServer.get('/', function(req, res){
-	res.redirect('/html/main.html');
+	sendFile("public/view/main.html", res, {title: "Main Page"});
 });
+
+function sendFile(file, res, headerOptions, footerOptions){
+	fileSystem.readFile(file, function(err, body){
+		var header = ejs.render(fileSystem.readFileSync('public/partial/header.ejs', 'utf-8'), headerOptions);
+		var footer = ejs.render(fileSystem.readFileSync('public/partial/footer.ejs', 'utf-8'), footerOptions);
+		var html = header + body + footer;
+		res.send(html);
+	});
+}
 
 webServer.listen(8080, function(){
 	console.log("Listening at localhost:8080");
