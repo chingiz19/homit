d853548e630af202e5d7ef28a0eadd22caf188cf
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2017-02-25 16:42:31 -0700

    Added structure for delegating routing to custom modules in app.js

--- app.js
+++ app.js
@@ -3,32 +3,19 @@ var webServer = express();
 var path = require("path");
 var webpagePath = path.join(__dirname, "/public");
 var bodyParser = require("body-parser");
-var fileSystem = require("fs");
-var ejs = require("EJS");
 
 
 
 /* Webpage is hosted at 8080 */
-
 webServer.use(bodyParser.json());
 webServer.use(bodyParser.urlencoded({ extended: true}));
 webServer.use(express.static(webpagePath));
-
 webServer.set('view engine', 'ejs');
 
-
-webServer.get('/', function(req, res){
-	sendFile("public/view/main.html", res, {title: "Main Page"});
-});
-
-function sendFile(file, res, headerOptions, footerOptions){
-	fileSystem.readFile(file, function(err, body){
-		var header = ejs.render(fileSystem.readFileSync('public/partial/header.ejs', 'utf-8'), headerOptions);
-		var footer = ejs.render(fileSystem.readFileSync('public/partial/footer.ejs', 'utf-8'), footerOptions);
-		var html = header + body + footer;
-		res.send(html);
-	});
-}
+/* Custom modules to use for proper routing */
+webServer.use("/", require(path.join(__dirname, "/view_controllers/generic_controller")));
+//TODO: Add when we get api calls
+// webServer.use("/api/", require("path to api"));
 
 webServer.listen(8080, function(){
 	console.log("Listening at localhost:8080");
---
+++ view_controllers/generic_controller.js
@@ -0,0 +1,26 @@
+var router = require("express").Router();
+var fileSystem = require("fs");
+var ejs = require("EJS");
+
+var headerEjs = fileSystem.readFileSync('public/partial/header.ejs', 'utf-8');
+var footerEjs = fileSystem.readFileSync('public/partial/footer.ejs', 'utf-8');
+
+router.get('/', function(req, res, next){
+	var body = fileSystem.readFileSync("public/view/main.html", 'utf-8');
+	res.headerOptions = {title: "Main Page"};
+	res.footerOptions = null;
+	res.body = body;
+	next();
+});
+
+/**
+ * Middleware to append header and footer
+ */
+router.use(function(req, res, next){
+		var header = ejs.render(headerEjs, res.headerOptions);
+		var footer = ejs.render(footerEjs, res.footerOptions);
+		var html = header + res.body + footer;
+		res.send(html);
+});
+
+module.exports = router;
