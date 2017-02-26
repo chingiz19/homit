918672c921ce8fb955570aa20798d7e9ba6fef0d
Zaman Zamanli <zaman_zamanli@hotmail.com>
2017-02-26 14:14:00 -0700

    New sql files

---
+++ project_setup/resources/db/sql1.sql
@@ -0,0 +1,6 @@
+CREATE USER 'delivery_user'@'localhost' IDENTIFIED BY 'ahmadtea';
+GRANT USAGE ON *.* TO 'delivery_user'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+
+CREATE DATABASE delivery_db;
+
+GRANT ALL PRIVILEGES ON `delivery\_db`.* TO 'delivery_user'@'localhost' WITH GRANT OPTION;
