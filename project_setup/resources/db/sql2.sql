804fb0818b6d0e8c9965de7aba2752ac91fda180
Zaman Zamanli <zaman_zamanli@hotmail.com>
2017-02-26 14:31:48 -0700

    New db settings

--- project_setup/resources/db/sql2.sql
+++ project_setup/resources/db/sql2.sql
@@ -1,3 +1,6 @@
+/*
+* This scripts creates tables for the database.
+*/
 CREATE TABLE `delivery_db`.`product_types` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(225) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 CREATE TABLE `delivery_db`.`product_list` ( `id` INT NOT NULL AUTO_INCREMENT , `product_type` INT NOT NULL , `product_name` VARCHAR(225) NOT NULL , `product_description` VARCHAR(225) NOT NULL , `product_price` DECIMAL(10, 2) NOT NULL , `product_image` VARCHAR(225) NOT NULL , PRIMARY KEY (`id`) , CONSTRAINT `fk_product_type` FOREIGN KEY (`product_type`) REFERENCES `product_types`(`id`) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE = InnoDB;
 CREATE TABLE `delivery_db`.`product_warehouse` ( `id` INT NOT NULL AUTO_INCREMENT , `product_id` INT NOT NULL , `quantity` INT NOT NULL , PRIMARY KEY (`id`) , CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `product_list`(`id`) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE = InnoDB;
