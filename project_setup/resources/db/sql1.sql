a6bb7e4837f1c423310fe657e3adb4c0b0844143
Zaman Zamanli <zaman_zamanli@hotmail.com>
2017-03-05 21:36:46 -0700

    Updated sql files

--- project_setup/resources/db/sql1.sql
+++ project_setup/resources/db/sql1.sql
@@ -1,7 +1,11 @@
 /*
-* This scripts creates a database and a user.
+* This scripts deletes user and database which we want to create. 
+* Then it creates a database and a user.
 * Grants all privileges for the database to the user.
 */
+DROP DATABASE IF EXISTS delivery_db;
+DROP USER IF EXISTS 'delivery_user'@'localhost';
+
 CREATE USER 'delivery_user'@'localhost' IDENTIFIED BY 'ahmadtea';
 GRANT USAGE ON *.* TO 'delivery_user'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 
