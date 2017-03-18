31af03694278a2cdc813239eb9b3bc7e2f825b59
Zaman Zamanli <zaman_zamanli@hotmail.com>
2017-03-18 14:30:47 -0600

    New db

--- api_controllers/catalog.js
+++ api_controllers/catalog.js
@@ -8,6 +8,7 @@ router.get('/beers', function(req, res, next){
             success: 'true',
             types: types,
             brands: "brands",
+            pacakgings: "pacakgings",
             beers: "all beers with details"
         };
         res.send(response);
@@ -40,7 +41,9 @@ var getAllBeerTypes = function() {
 };
 
 var getAllWinesTypes = function() {
-    return getTypes(categories.Wines);
+    return getTypes(categories.Wines).then(function(wines) {
+        return wines;
+    });
 };
 
 var getTypes = function(category_id) {
@@ -49,11 +52,33 @@ var getTypes = function(category_id) {
     var result = [];
     return db.selectQuery(types, data).then(function(dbResult) {
         for (i = 0; i < dbResult.length; i++) { 
-            result[i] = dbResult[i].name;
+            result.push(dbResult[i].name);
         }
         return result;
     });
 };
+
+// SELECT 
+// w.id AS id, 
+// w.product_id AS "Product Id",
+// w.price AS price,
+// w.quantity AS quantity,
+// pa.name AS packaging,
+// pr.product_brand AS brand,
+// pr.product_name AS name,
+// pr.product_description AS description,
+// t.name AS type,
+// c.name AS category
+// FROM catalog_warehouse AS w,
+// catalog_packagings AS pa,
+// catalog_products AS pr,
+// catalog_types AS t,
+// catalog_categories AS c
+// WHERE 1=1
+// AND w.packaging_id = pa.id
+// AND w.product_id = pr.id
+// AND pr.type_id = t.id
+// AND t.category_id = c.id
 
 
 module.exports = router;
