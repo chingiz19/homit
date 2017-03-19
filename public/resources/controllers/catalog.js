581e7a9a70216e195b295604a3e3413eba2315bb
LAPTOP-H28DMTV2\ching <chingiz.baxishov@gmail.com>
2017-03-19 01:43:37 -0600

    Some work done on catalog

--- public/resources/controllers/catalog.js
+++ public/resources/controllers/catalog.js
@@ -1,5 +1,17 @@
 app.controller("catalogController", function($scope, $http) {
     $scope.selection = "Beers"
+    $scope.types = ['ale', 'lager', 'pilsner', 'IPA']
+    $scope.packings = ['6-pack', '8-pack', '12-pack', '15-pack']
+    $scope.products = [
+        { type: 'ale', packing: '6-pack', brand: 'Big Rock' },
+        { type: 'lager', packing: '12-pack', brand: 'Canadian' },
+        { type: 'IPA', packing: '8-pack', brand: 'Wild Rose' },
+        { type: 'lager', packing: '6-pack', brand: 'Canadian' },
+        { type: 'lager', packing: '15-pack', brand: 'Canadian' },
+        { type: 'ale', packing: '12-pack', brand: 'Wild Rose' },
+        { type: 'pilsner', packing: '6-pack', brand: 'Pilsner' },
+        { type: 'lager', packing: '15-pack', brand: 'Old Milwaukee' },
+    ]
 
 
 });
