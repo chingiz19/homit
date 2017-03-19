71e9f5446a6a147dc058e8edb3c9d38d3961792b
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2017-03-19 08:58:00 -0600

    Improvement to myaccount

--- public/resources/controllers/myaccount.js
+++ public/resources/controllers/myaccount.js
@@ -1,5 +1,24 @@
-app.controller("myaccountController", function($scope, $http) {
-    $scope.sidebarOption = function(section){
-        $("#" + section).removeClass("hidden");
+app.controller("myaccountController", ["$scope", function($scope){
+    $scope.newPassword = "";
+    $scope.confirmPassword = "";
+    $scope.showError = false;
+    $scope.matchPassword = function(){
+        var nPass = $("#nPassword");
+        var cPass = $("#cPassword");
+        var errorClass = "genericError";
+        // check that none is edited
+        if (nPass.hasClass("ng-dirty") && cPass.hasClass("ng-dirty")){
+            // add error-class if values do not match
+            if ($scope.newPassword != $scope.confirmPassword){
+                cPass.addClass(errorClass);
+                $scope.showError = true;
+            } else {
+                cPass.removeClass(errorClass);
+                $scope.showError = false;
+            }
+        } else {
+            cPass.removeClass(errorClass);
+            $scope.showError = false;
+        }
     }
-});
+}]);
