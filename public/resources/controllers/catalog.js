e0a89b804c1b5b0f7a59088aa4c77c4da42ed5c0
Chingiz Bakhishov <chingiz.baxishov@gmail.com>
2017-03-20 02:44:53 -0600

    Navigation section of filter is done.Open console on the side to see results.

--- public/resources/controllers/catalog.js
+++ public/resources/controllers/catalog.js
@@ -1,17 +1,53 @@
 app.controller("catalogController", function($scope, $http) {
-    $scope.selection = "Beers"
-    $scope.types = ['ale', 'lager', 'pilsner', 'IPA']
-    $scope.packings = ['6-pack', '8-pack', '12-pack', '15-pack']
+    $scope.selection = "Beers";
+    $scope.types = ['ale', 'lager', 'pilsner', 'IPA'];
+    $scope.packings = ['6-pack', '8-pack', '12-pack', '15-pack'];
     $scope.products = [
-        { type: 'ale', packing: '6-pack', brand: 'Big Rock' },
-        { type: 'lager', packing: '12-pack', brand: 'Canadian' },
-        { type: 'IPA', packing: '8-pack', brand: 'Wild Rose' },
-        { type: 'lager', packing: '6-pack', brand: 'Canadian' },
-        { type: 'lager', packing: '15-pack', brand: 'Canadian' },
-        { type: 'ale', packing: '12-pack', brand: 'Wild Rose' },
-        { type: 'pilsner', packing: '6-pack', brand: 'Pilsner' },
-        { type: 'lager', packing: '15-pack', brand: 'Old Milwaukee' },
-    ]
+        { type: 'ale', packing: '6-pack', brand: 'Big Rock', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
+        { type: 'lager', packing: '12-pack', brand: 'Canadian', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
+        { type: 'IPA', packing: '8-pack', brand: 'Wild Rose', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
+        { type: 'lager', packing: '6-pack', brand: 'Canadian', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
+        { type: 'lager', packing: '15-pack', brand: 'Canadian', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
+        { type: 'ale', packing: '12-pack', brand: 'Wild Rose', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
+        { type: 'pilsner', packing: '6-pack', brand: 'Pilsner', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
+        { type: 'lager', packing: '15-pack', brand: 'Old Milwaukee', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
+    ];
+    $scope.filter = [];
 
+    $scope.stringfinder = function(str) {
+        var j = 0;
+        var k = 0;
+        if ($scope.filter.length === 0) { return -1; } else {
+            while (j < $scope.filter.length) {
+                if ($scope.filter[j] == str) {
+                    k = j;
+                    j++;
+                } else {
+                    k = -1;
+                    j++;
+                }
+            }
+            return k;
+        }
+
+
+    }
+
+    $scope.addtofilter = function(name) {
+        alert('clicked');
+        var a = $scope.stringfinder(name);
+        console.log(a);
+
+        if (a < 0) {
+            $scope.filter.push(name);
+            alert('addeditem');
+            console.log($scope.filter);
+        } else {
+            delete $scope.filter[a];
+            alert('itemdeleted');
+            console.log($scope.filter);
+        }
+
+    };
 
 });
