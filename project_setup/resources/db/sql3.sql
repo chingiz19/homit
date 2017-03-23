c6c21e2c3eb7bf9bbb718d4f9b562470eb9c6b49
Zaman Zamanli <zaman_zamanli@hotmail.com>
2017-03-22 23:54:59 -0600

    New db New catalog.js

--- project_setup/resources/db/sql3.sql
+++ project_setup/resources/db/sql3.sql
@@ -1,7 +1,7 @@
 /* Inserts dummy data into tables */
-INSERT INTO `delivery_db`.`catalog_categories` (`id`, `name`) VALUES (NULL, 'Beers'), (NULL, 'Wines'), (NULL, 'Spirits'), (NULL, 'Others');
-INSERT INTO `delivery_db`.`catalog_subcategories` (`id`, `name`, `category_id`) VALUES (NULL, 'Red', '2'), (NULL, 'White', '2');
-INSERT INTO `delivery_db`.`catalog_types` (`id`, `name`, `category_id`, `subcategory_id`) VALUES (NULL, 'Ales', '1', NULL), (NULL, 'Lagers', '1', NULL), (NULL, 'Stouts & Porters', '1', NULL), (NULL, 'Malts', '1', NULL);
+INSERT INTO `delivery_db`.`catalog_categories` (`id`, `name`) VALUES ('1', 'Beers'), ('2', 'Wines'), ('3', 'Spirits'), ('4', 'Others');
+INSERT INTO `delivery_db`.`catalog_subcategories` (`id`, `name`, `category_id`) VALUES (NULL, 'Beer', '1'), (NULL, 'Red', '2'), (NULL, 'White', '2'), (NULL, 'Brandy', '3'), (NULL, 'Rum', '4');
+INSERT INTO `delivery_db`.`catalog_types` (`id`, `name`, `subcategory_id`) VALUES (NULL, 'Ales', '1'), (NULL, 'Lagers', '1'), (NULL, 'Stouts & Porters', '1'), (NULL, 'Malts', '1');
 INSERT INTO `delivery_db`.`catalog_packagings` (`id`, `name`, `category_id`) VALUES (NULL, '6 pack', '1'), (NULL, '12 pack', '1'), (NULL, '24 pack', '1'), (NULL, '48 pack', '1');
 INSERT INTO `delivery_db`.`catalog_products` (`id`, `product_brand`, `product_name`, `product_description`, `product_image`, `type_id`) VALUES (NULL, 'Canadian', NULL, 'This beer is good.', 'image', '1'), (NULL, 'Miller', 'Pive', 'This beer is good as well.', 'image', '2');
 INSERT INTO `delivery_db`.`catalog_warehouse` (`id`, `packaging_id`, `price`, `product_id`, `quantity`) VALUES (NULL, '1', '32.12', '1' , '43'), (NULL, '3', '12.23', '2', '44');
