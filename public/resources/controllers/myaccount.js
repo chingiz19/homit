a7690832e94902844835d09ace022484ec62136d
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2017-03-23 11:27:04 -0600

    Finished Front-End of myaccount page

--- public/resources/controllers/myaccount.js
+++ public/resources/controllers/myaccount.js
@@ -1,26 +1,52 @@
 app.controller("myaccountController", ["$scope", function($scope){
-    $scope.newPassword = "";
-    $scope.confirmPassword = "";
+    
+    /* Info Section */
+    //TODO: init with http call when page is loaded
+    $scope.firstname = "Badass";
+    $scope.lastname = "Team";
+
+    //variables
+    var initialFirstName = "Badass";
+    var initialLastName = "Team";
+
+    $scope.registerChanges = function(){
+        // enable change request is any of the boxes has new values
+        if ($scope.firstname == initialFirstName && $scope.lastname == initialLastName){
+            return true;    
+        }
+        return false;
+    }
+    
+    
+    /* Password section */
     $scope.showError = false;
-    $scope.enablePasswordChange = false;
-    $scope.matchPassword = function(){
-        var nPass = $("#nPassword");
-        var cPass = $("#cPassword");
-        var errorClass = "genericError";
-        // check that none is edited
-        if (nPass.hasClass("ng-dirty") && cPass.hasClass("ng-dirty")){
-            // add error-class if values do not match
-            if ($scope.newPassword != $scope.confirmPassword){
-                cPass.addClass(errorClass);
-                $scope.showError = true;
-                $scope.enablePasswordChange = false;
-            } else {
-                cPass.removeClass(errorClass);
-                $scope.showError = false;
-                $scope.enablePasswordChange = true;
-            }
+    $scope.passwordMatch = false;
+
+    // variables
+    var errorClass = "genericError";
+    
+    $scope.enablePasswordChanges = function(){
+        if ($scope.passwordSectionForm.$pristine){
+            return false;
+        }
+
+        if ($scope.showError && $scope.passwordSectionForm.$valid){
+            return true;
+        }
+
+        return false;
+    } 
+
+    $scope.passwordMatched = function(){
+
+        if (!$scope.confirmPassword){
+            $scope.showError = false;
+            return;
+        }
+        
+        if ($scope.newPassword != $scope.confirmPassword){
+            $scope.showError = true;
         } else {
-            cPass.removeClass(errorClass);
             $scope.showError = false;
         }
     }
