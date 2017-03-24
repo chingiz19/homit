5c7f12922273187215e962654534b3846dba5bbc
Zaman Zamanli <zaman_zamanli@hotmail.com>
2017-03-23 23:14:04 -0600

    changes to frontend catalog

--- public/resources/controllers/catalog.js
+++ public/resources/controllers/catalog.js
@@ -1,60 +1,121 @@
 app.controller("catalogController", function($scope, $http) {
     $scope.selection = "Beers";
-    $scope.types = ['ale', 'lager', 'pilsner', 'IPA'];
-    $scope.packings = ['6-pack', '8-pack', '12-pack', '15-pack'];
-    $scope.products = [
-        { type: 'ale', packing: '6-pack', brand: 'Big Rock', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
-        { type: 'lager', packing: '12-pack', brand: 'Canadian', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
-        { type: 'IPA', packing: '8-pack', brand: 'Wild Rose', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
-        { type: 'lager', packing: '6-pack', brand: 'Canadian', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
-        { type: 'lager', packing: '15-pack', brand: 'Canadian', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
-        { type: 'ale', packing: '12-pack', brand: 'Wild Rose', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
-        { type: 'pilsner', packing: '6-pack', brand: 'Pilsner', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
-        { type: 'lager', packing: '15-pack', brand: 'Old Milwaukee', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
-    ];
-    $scope.filter = [];
-    $scope.stringfinder = function(str) {
-        var k = -1;
-        if ($scope.filter.length === 0) {
-            return k;
-        } else {
-            for (j = 0; j < $scope.filter.length; j++) {
-                if ($scope.filter[j] == str) {
-                    k = j;
-                }
-            }
-        }
-        return k;
+    // Determine product URL
+    $scope.productUrl;
+    if ($scope.selection == "Beers") {
+        $scope.productUrl = '/api/catalog/beers';
     }
-
-
-    $scope.addtofilter = function(name) {
-        alert('adde to filter');
-        var a = $scope.stringfinder(name);
-        console.log(a);
-        if (a < 0) {
-            $scope.filter.push(name);
-            console.log($scope.filter);
+    // Get initial list of products
+    $scope.products;
+    $scope.subcategories;
+    $scope.packings;
+    $scope.brands;
+    $http({
+        method: 'GET',
+        url: $scope.productUrl
+    }).then(function successCallback(response) {
+        if (response.data['success'] === "true") {
+            $scope.products = response.data['products'];
+            $scope.subcategories = response.data['subcategories'];
+            $scope.packings = response.data['packagings'];
+            $scope.brands = response.data['brands'];
         } else {
-            $scope.filter.splice(a, 1);
-            console.log($scope.filter);
+
+        }
+    }, function errorCallback(response) {
+
+    });
+
+    $scope.userSelectedSubcategories = [];
+    $scope.userSelectedTypes = [];
+    $scope.userSelectedBrands = [];
+    $scope.userSelectedPackings = [];
+
+    $scope.checkSubcategories = function(subcategory) {
+        if (!$scope.userSelectedSubcategories.includes(subcategory)) {
+            $scope.userSelectedSubcategories.push(subcategory);
+        } else {
+            $scope.userSelectedSubcategories.splice($scope.userSelectedSubcategories.indexOf(subcategory), 1);
+        }
+        console.log("userSelectedSubcategories list is: " + $scope.userSelectedSubcategories);
+    };
+
+    $scope.checkTypes = function(type) {
+        if (!$scope.userSelectedTypes.includes(type)) {
+            $scope.userSelectedTypes.push(type);
+        } else {
+            $scope.userSelectedTypes.splice($scope.userSelectedTypes.indexOf(type), 1);
+        }
+        console.log("userSelectedTypes list is: " + $scope.userSelectedTypes);
+    };
+
+    $scope.checkBrands = function(brand) {
+        if (!$scope.userSelectedBrands.includes(brand)) {
+            $scope.userSelectedBrands.push(brand);
+        } else {
+            $scope.userSelectedBrands.splice($scope.userSelectedBrands.indexOf(brand), 1);
+        }
+        console.log("userSelectedBrands list is: " + $scope.userSelectedBrands);
+    };
+
+    $scope.checkPackings = function(packing) {
+        if (!$scope.userSelectedPackings.includes(packing)) {
+            $scope.userSelectedPackings.push(packing);
+        } else {
+            $scope.userSelectedPackings.splice($scope.userSelectedPackings.indexOf(packing), 1);
+        }
+        console.log("userSelectedPackings list is: " + $scope.userSelectedPackings);
+    };
+
+    $scope.isInSelectedSubcategories = function(subcategory) {
+        if ($scope.userSelectedSubcategories.length === 0) {
+            return true;
+        } else {
+            if ($scope.userSelectedSubcategories.includes(subcategory)) {
+                return true;
+            } else {
+                return false;
+            }
         }
     };
 
-
-    $scope.isinfilter = function(name) {
-        var a = $scope.filter;
-        if (a.length === 0) {
-            console.log('first if state');
+    $scope.isInSelectedTypes= function(type) {
+        if ($scope.userSelectedTypes.length === 0) {
+            console.log("truee");
             return true;
-        } else if (a.indexOf(name) === -1) {
-            console.log('else if state');
-            return false;
         } else {
-            console.log('else state');
-            return true;
+            if ($scope.userSelectedTypes.includes(type)) {
+                console.log("in the list");
+                return true;
+            } else {
+                console.log("NOT IN THE LIST");
+                return false;
+            }
         }
-    }
+    };
 
+    $scope.isInSelectedBrands = function(brand) {
+        if ($scope.userSelectedBrands.length === 0) {
+            return true;
+        } else {
+            if ($scope.userSelectedBrands.includes(brand)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+    $scope.isInSelectedPackings = function(packing) {
+        if ($scope.userSelectedPackings.length === 0) {
+            return true;
+        } else {
+            if ($scope.userSelectedPackings.includes(packing)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
 
 });
