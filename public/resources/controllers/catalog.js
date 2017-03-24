b886e19c92e4cb0ed134e7b5d2ac6dfd2ac33ab5
Chingiz Bakhishov <chingiz.baxishov@gmail.com>
2017-03-23 21:13:07 -0600

    lmost done with filtration part

--- public/resources/controllers/catalog.js
+++ public/resources/controllers/catalog.js
@@ -13,41 +13,48 @@ app.controller("catalogController", function($scope, $http) {
         { type: 'lager', packing: '15-pack', brand: 'Old Milwaukee', url: 'http://www.totalwine.com/media/sys_master/twmmedia/h60/hcb/8804652056606.png', price: '15 CAD' },
     ];
     $scope.filter = [];
-
     $scope.stringfinder = function(str) {
-        var j = 0;
-        var k = 0;
-        if ($scope.filter.length === 0) { return -1; } else {
-            while (j < $scope.filter.length) {
+        var k = -1;
+        if ($scope.filter.length === 0) {
+            return k;
+        } else {
+            for (j = 0; j < $scope.filter.length; j++) {
                 if ($scope.filter[j] == str) {
                     k = j;
-                    j++;
-                } else {
-                    k = -1;
-                    j++;
                 }
             }
-            return k;
         }
-
-
+        return k;
     }
 
+
     $scope.addtofilter = function(name) {
-        alert('clicked');
+        alert('adde to filter');
         var a = $scope.stringfinder(name);
         console.log(a);
-
         if (a < 0) {
             $scope.filter.push(name);
-            alert('addeditem');
             console.log($scope.filter);
         } else {
-            delete $scope.filter[a];
-            alert('itemdeleted');
+            $scope.filter.splice(a, 1);
             console.log($scope.filter);
         }
-
     };
+
+
+    $scope.isinfilter = function(name) {
+        var a = $scope.filter;
+        if (a.length === 0) {
+            console.log('first if state');
+            return true;
+        } else if (a.indexOf(name) === -1) {
+            console.log('else if state');
+            return false;
+        } else {
+            console.log('else state');
+            return true;
+        }
+    }
+
 
 });
