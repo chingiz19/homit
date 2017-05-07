ff6dafb2c290949edd3fbe8257ba50380c3b63c4
Zaman Zamanli <zaman_zamanli@hotmail.com>
2017-05-07 15:53:37 -0600

    Added sorting for catalog b.e, added some documentation

--- api_controllers/catalog.js
+++ api_controllers/catalog.js
@@ -78,30 +78,45 @@ var categories = {
   Others: 4
 };
 
+/**
+ * Gets all types for beers
+ */
 var getAllBeerTypes = function() {
     return getTypes(categories.Beers).then(function(beers) {
         return beers;
     });
 };
 
+/**
+ * Gets all types for wines
+ */
 var getAllWineTypes = function() {
     return getTypes(categories.Wines).then(function(wines) {
         return wines;
     });
 };
 
+/**
+ * Gets all types for spirits
+ */
 var getAllSpiritTypes = function() {
     return getTypes(categories.Spirits).then(function(spirits) {
         return spirits;
     });
 };
 
+/**
+ * Gets all types for others
+ */
 var getAllOtherTypes = function() {
     return getTypes(categories.Others).then(function(others) {
         return others;
     });
 };
 
+/**
+ * Get all types based for the category provided
+ */
 var getTypes = function(category_id) {
     var sqlQuery = `SELECT s.name AS subcategory, t.name AS type FROM catalog_categories AS c,
         catalog_subcategories AS s, catalog_types AS t
@@ -140,30 +155,45 @@ var getTypes = function(category_id) {
     });
 };
 
+/**
+ * Return all beers
+ */
 var getAllBeers = function() {
     return getAllProducts(categories.Beers).then(function(beers) {
         return beers;
     });
 };
 
+/**
+ * Return all wines
+ */
 var getAllWines = function() {
     return getAllProducts(categories.Wines).then(function(wines) {
         return wines;
     });
 };
 
+/**
+ * Return all spirits
+ */
 var getAllSpirits = function() {
     return getAllProducts(categories.Spirits).then(function(spirits) {
         return spirits;
     });
 };
 
+/**
+ * Return all others
+ */
 var getAllOthers = function() {
     return getAllProducts(categories.Others).then(function(others) {
         return others;
     });
 };
 
+/**
+ * Return all products based on the category provided
+ */
 var getAllProducts = function(category_id) {
     var sqlQuery = `SELECT w.id AS warehouse_id, w.product_id AS product_id, s.name AS subcategory, 
         t.name AS type, pr.product_brand AS brand, pr.product_name AS name, pr.product_description AS description,
@@ -178,6 +208,9 @@ var getAllProducts = function(category_id) {
     });
 };
 
+/**
+ * Gets all brands for the products provided
+ */
 var getAllBrands = function (products) {
     var result = [];
     for (i = 0; i < products.length; i++) {
@@ -185,9 +218,12 @@ var getAllBrands = function (products) {
             result.push(products[i].brand);
         }
     }
-    return result;
+    return result.sort();
 };
 
+/**
+ * Gets all packagings for the products provided
+ */
 var getAllPackagings = function (products) {
     var result = [];
     for (i = 0; i < products.length; i++) {
@@ -195,8 +231,27 @@ var getAllPackagings = function (products) {
             result.push(products[i].packaging);
         }
     }
-    return result;
+    return result.sort(sortAlphaNum);
 };
+
+/**
+ * Custom function to do alphanumeric sort
+ * 
+ * Source: http://stackoverflow.com/questions/4340227/sort-mixed-alpha-numeric-array
+ */
+var reA = /[^a-zA-Z]/g;
+var reN = /[^0-9]/g;
+function sortAlphaNum(a,b) {
+    var aA = a.replace(reA, "");
+    var bA = b.replace(reA, "");
+    if(aA === bA) {
+        var aN = parseInt(a.replace(reN, ""), 10);
+        var bN = parseInt(b.replace(reN, ""), 10);
+        return aN === bN ? 0 : aN > bN ? 1 : -1;
+    } else {
+        return aA > bA ? 1 : -1;
+    }
+}
 
 
 module.exports = router;
