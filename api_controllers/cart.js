e19bfb267d3d09576fd2d143fe0c622528063388
Zaman Zamanli <zaman_zamanli@hotmail.com>
2017-06-01 20:33:59 -0600

    Added method stubs for cart api calls

---
+++ api_controllers/cart.js
@@ -0,0 +1,18 @@
+var router = require("express").Router();
+var db = require("../db.js");
+
+router.get('/usercart', function(req, res, next){
+
+});
+
+router.post('/addproduct', function(req, res, next){
+
+});
+
+router.post('/removeproduct', function(req, res, next){
+
+});
+
+router.post('/clear', function(req, res, next){
+
+});
