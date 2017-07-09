8c7471930f3c7fe9eb5f8c7e855c4f0a36c6efbf
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2017-07-09 16:27:06 -0600

    Moving cartBox from header to catalog page

---
+++ public/resources/controllers/checkout_menu.js
@@ -0,0 +1,143 @@
+app.controller("cartController", function ($scope, $sce, $rootScope, $http) {
+    $scope.userCart = {};
+    $scope.numberOfItemsInCart = 0;
+    $scope.totalAmount = 0;
+
+    $http({
+        method: 'GET',
+        url: 'api/cart/usercart'
+    }).then(function successCallback(response) {
+        if (response.data['success'] === true) {
+            $scope.userCart = response.data['cart'];
+            for(var a in $scope.userCart){
+                $scope.tempTotalAmount=$scope.userCart[a]['quantity']*$scope.userCart[a]['price'];
+                $scope.totalAmount=$scope.totalAmount+$scope.tempTotalAmount;
+                $scope.tempNumberOfItemsInCart=$scope.userCart[a]['quantity'];
+                $scope.numberOfItemsInCart=$scope.numberOfItemsInCart+$scope.tempNumberOfItemsInCart;
+            }
+        } else {
+        }
+    }, function errorCallback(response) {
+        console.log("error");
+        console.log(response);
+    });
+
+    $scope.$on("addToCart", function (event, args) {
+        var tmp = 1;
+        if ($scope.userCart.hasOwnProperty(args.addedProduct.warehouse_id)) {
+            tmp = $scope.userCart[args.addedProduct.warehouse_id]["quantity"];
+            tmp++;
+            if (tmp >= 10) tmp = 10;
+            else {
+                $scope.userCart[args.addedProduct.warehouse_id]["quantity"] = tmp;
+                $scope.numberOfItemsInCart++;
+                $scope.totalAmount = $scope.totalAmount + args.addedProduct.price;
+            }
+        } else {
+            $scope.userCart[args.addedProduct.warehouse_id] = args.addedProduct;
+            $scope.userCart[args.addedProduct.warehouse_id]["quantity"] = tmp;
+            $scope.numberOfItemsInCart++;
+            $scope.totalAmount = $scope.totalAmount + args.addedProduct.price;
+        }
+        $scope.prepareItemForDB(args.addedProduct.warehouse_id, tmp);
+    })
+
+    $scope.plusItem = function (product) {
+        var tmp = 1;
+        if ($scope.userCart.hasOwnProperty(product.warehouse_id)) {
+            tmp = $scope.userCart[product.warehouse_id]["quantity"];
+            tmp++;
+            if (tmp >= 10) tmp = 10;
+            else {
+                $scope.userCart[product.warehouse_id]["quantity"] = tmp;
+                $scope.numberOfItemsInCart++;
+                $scope.totalAmount = $scope.totalAmount + product.price;
+            }
+        }
+        $scope.prepareItemForDB(product.warehouse_id, tmp);
+    }
+
+    $scope.minusItem = function (product) {
+        var tmp = 1;
+        if ($scope.userCart.hasOwnProperty(product.warehouse_id) && $scope.userCart[product.warehouse_id]["quantity"] >= 1) {
+            tmp = $scope.userCart[product.warehouse_id]["quantity"];
+            tmp--;
+            if (tmp < 1) tmp = 1;
+            else {
+                $scope.userCart[product.warehouse_id]["quantity"] = tmp;
+                $scope.numberOfItemsInCart--;
+                if ($scope.numberOfItemsInCart < 0) $scope.numberOfItemsInCart = 0;
+                $scope.totalAmount = $scope.totalAmount - product.price;
+                if ($scope.totalAmount < 0) $scope.totalAmount = 0;
+            }
+        }
+        $scope.prepareItemForDB(product.warehouse_id, tmp);
+    }
+
+    $scope.clearCart = function (product) {
+        $scope.userCart = {};
+        $scope.numberOfItemsInCart = 0;
+        $scope.totalAmount = 0;
+
+        $http({
+            method: 'POST',
+            url: '/api/cart/clear',
+        }).then(function successCallback(response) {
+            if (!response.data["error"]) {
+            } else {
+            }
+        }, function errorCallback(response) {
+            console.log("ERROR");
+        });
+    }
+
+    $scope.removeFromCart = function (product) {
+        if ($scope.userCart.hasOwnProperty(product.warehouse_id)) {
+            var tmp = 0;
+            delete $scope.userCart[product.warehouse_id];
+            $scope.numberOfItemsInCart = $scope.numberOfItemsInCart - product.quantity;
+            $scope.totalAmount = $scope.totalAmount - product.price * product.quantity;
+            $scope.prepareItemForDB(product.warehouse_id, tmp);
+        }
+    }
+
+    $scope.prepareItemForDB = function (warehouse_id, itemQuantity, action) {
+        $scope.addItemToUserDB = {};
+        $scope.addItemToUserDB["warehouse_id"] = warehouse_id;
+        $scope.addItemToUserDB["quantity"] = itemQuantity;
+        $scope.addItemToUserDB["action"] = action;
+
+        $http({
+            method: 'POST',
+            url: '/api/cart/modifyitem',
+            data: {
+                warehouse_id: $scope.addItemToUserDB["warehouse_id"],
+                quantity: $scope.addItemToUserDB["quantity"],
+            }
+        }).then(function successCallback(response) {
+            if (!response.data["error"]) {
+            } else {
+            }
+        }, function errorCallback(response) {
+            console.log("ERROR");
+        });
+
+        $http({
+            method: 'GET',
+            url: 'api/cart/usercart'
+        }).then(function successCallback(response) {
+            if (response.data['success'] === true) {
+                $scope.userCart = response.data['cart'];
+            } else {
+            }
+        }, function errorCallback(response) {
+            console.log("error");
+            console.log(response);
+        });
+
+    }
+});
+
+$("#cart").click(function(){
+    $(".sideMenu").toggleClass("show");
+})
