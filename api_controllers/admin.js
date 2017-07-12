d2d59af332f34987a33e55c81107bfb179092092
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2017-07-11 20:44:44 -0600

    Added admin.js API controler

---
+++ api_controllers/admin.js
@@ -0,0 +1,8 @@
+var router = require("express").Router();
+var db = require("../db.js");
+
+router.get('/', function(req, res, next){
+
+});
+
+module.exports = router;
--- api_controllers/generic_controller.js
+++ api_controllers/generic_controller.js
@@ -11,5 +11,6 @@ router.use("/authentication", require(path.join(__dirname, "./authentication")))
 router.use("/catalog", require(path.join(__dirname, "./catalog")));
 router.use("/myaccount", require(path.join(__dirname, "./myaccount")));
 router.use("/cart", require(path.join(__dirname, "./cart")));
+router.use("/admin", require(path.join(__dirname, "./admin")));
 
 module.exports = router;
