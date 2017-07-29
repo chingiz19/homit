c0c6d4354c56cc8ffe85f6e5b9b3015956c991b3
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2017-07-29 12:34:12 -0600

    Loading orders in admin.ejs page using api call to get all orders

--- public/resources/controllers/admin.js
+++ public/resources/controllers/admin.js
@@ -1,6 +1,27 @@
+app.filter('totalPrice', function() {
+    return function(input) {
+        var totalPrice = 0;
+        input.forEach(function(cart_item){
+            var price = cart_item["price"];
+            var quantity = parseInt(cart_item["quantity"]);
+            totalPrice += (price * quantity);
+        });
+        return totalPrice;
+    };
+});
+
 app.controller("adminController", ["$location", "$scope", "$cookies", "$http", "$rootScope", 
     function($location, $scope, $cookies, $http, $rootScope) {
 
 
         $scope.orders = [];
+
+    $http({
+        method: 'GET',
+        url: "/api/orders/getorder"
+    }).then(function successCallback(response) {
+        $scope.orders = response.data;
+    }, function errorCallback(response) {
+
+    });
 }]);
