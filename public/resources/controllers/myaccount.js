2e434e1adb6ad3934377f21463747ad0d12cf883
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2017-08-05 12:59:06 -0600

    Users can update their info through MyAccount page

--- public/resources/controllers/myaccount.js
+++ public/resources/controllers/myaccount.js
@@ -1,23 +1,35 @@
-app.controller("myaccountController", ["$scope", "$http", function($scope, $http){
+app.controller("myaccountController", ["$scope", "$http", "$cookies", "$rootScope", 
+function($scope, $http, $cookies, $rootScope){
     
     /* Info Section */
-    //TODO: init with http call when page is loaded
-    $scope.firstname = "Badass";
-    $scope.lastname = "Team";
-
-    //variables
-    var initialFirstName = "Badass";
-    var initialLastName = "Team";
-
-    $scope.registerChanges = function(){
-        // enable change request is any of the boxes has new values
-        if ($scope.firstname == initialFirstName && $scope.lastname == initialLastName){
-            return true;    
-        }
-        return false;
+    $scope.user = JSON.parse( $cookies.get("user").replace("j:", ""));  
+    
+    $scope.updateUserInfo = function(){
+            $http({
+                method: 'POST',
+                url: '/api/myaccount/updateUserInfo',
+                data: $scope.user
+            }).then(function successCallback(response) {
+                if (response.data["success"]) {
+                    $rootScope.$broadcast("addNotification", { 
+                        type: "alert-success", 
+                        message: response.data["ui_message"]
+                    });
+                    $scope.user = response.data.user;
+                } else {
+                    $rootScope.$broadcast("addNotification", { 
+                        type: "alert-danger", 
+                        message: response.data["ui_message"]
+                    });
+                }
+            }, function errorCallback(response) {
+                var m = response.data["ui_message"] || "Something went wrong while updating your info, please try again";
+                $rootScope.$broadcast("addNotification", { type: "alert-danger", message: m});
+                console.log("ERROR in password reset");
+            });
     }
-    
-    
+
+
     /* Password section */
     $scope.showError = false;
     $scope.passwordMatch = false;
@@ -51,11 +63,27 @@ app.controller("myaccountController", ["$scope", "$http", function($scope, $http
         }
     }
 
-    $scope.singout = function(){
-        $http.post({
-            url: "/api/singout"
-        }).then(function(success){
-            
-        });
+    $scope.signout = function(){
+            $http({
+                method: 'POST',
+                url: '/api/authentication/signout'
+            }).then(function successCallback(response) {
+                if (response.data["success"]) {
+                    //delete cookie
+                    $cookies.remove("user");
+                    $rootScope.$broadcast("addNotification", { 
+                        type: "alert-success", 
+                        message: response.data["ui_message"], 
+                        href: "/", 
+                        reload: true 
+                    });
+                } else {
+                    // TODO: error handling
+                    console.log("password not reset");
+                }
+            }, function errorCallback(response) {
+                $rootScope.$broadcast("addNotification", { type: "alert-danger", message: response.data["ui_message"]});
+                console.log("ERROR in password reset");
+            });
     }
 }]);
