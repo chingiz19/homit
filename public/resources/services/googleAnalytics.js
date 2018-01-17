118e1b5b426c23b036c34a85156699d84fdbeec8
Jeyhun Gurbanov <jeyhun.j.gurbanov@gmail.com>
2018-01-17 02:09:45 +0000

    Merged in gAnalytics (pull request #207)
    Adding google analytics and googleAnalytics service
    
    Approved-by: Zaman Zamanli <zaman_zamanli@hotmail.com>
    Approved-by: Chingiz Bakhishov <chingiz.bakhishov@gmail.com>

---
+++ public/resources/services/googleAnalytics.js
@@ -0,0 +1,26 @@
+/**
+ * This service provides helpers for Google Analytics 
+ * 
+ * 
+ * @copyright Homit
+ * @author Jeyhun Gurbanov
+ */
+app.service('notification', function(){
+    var pub = {};
+
+     /**
+     * Registers event with Google Analytics
+     * 
+     * @param {*} eventName - event name (REQUIRED)
+     * @param {*} params - event params (if any)
+     */
+    pub.addEvent = function(eventName, params){
+        if (params){
+            gtag('event', eventName, params);
+        } else {
+            gtag('event', eventName);
+        }
+    }
+
+    return pub;
+});
