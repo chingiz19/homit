2657c8621882d385df7af71f3c75e33588d6be01
Jeyhun Gurbanov <jgurbanov@homit.ca>
2018-01-24 00:20:07 +0000

    Merged in searchBar (pull request #237)
    Adding search bar to myaccount page
    
    Approved-by: Elnar Sharifli <elnarsharifli@gmail.com>

--- view_controllers/viewAuth_controller.js
+++ view_controllers/viewAuth_controller.js
@@ -2,7 +2,6 @@ var router = require("express").Router();
 
 router.get('/myaccount', Auth.validate({redirect: true}), function(req, res, next){
     req.options.ejs["title"] = "My Account";
-    req.options.ejs["showSearchBar"] = false;
 	res.render("myaccount.ejs", req.options.ejs);
 });
 
