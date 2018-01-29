3d51489237ca4659cc735e30e72963344597b8d2
Zaman Zamanli <zaman_zamanli@hotmail.com>
2018-01-28 17:21:10 -0700

    Add db_viewer user

--- project_setup/resources/db/production/0_create_database.sql
+++ project_setup/resources/db/production/0_create_database.sql
@@ -7,9 +7,12 @@
 */
 DROP DATABASE IF EXISTS homit;
 DROP USER IF EXISTS 'db_web_user'@'localhost';
+DROP USER IF EXISTS 'db_viewer'@'localhost';
 
 CREATE USER 'db_web_user'@'localhost' IDENTIFIED BY 'gg:(@SH%H3K5^4#P';
+CREATE USER 'db_viewer'@'localhost' IDENTIFIED BY 'gg:(@SH%H3K5^4#P';
 
 CREATE DATABASE homit;
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON `homit`.* TO 'db_web_user'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON `homit`.* TO 'db_viewer'@'localhost' WITH GRANT OPTION;
