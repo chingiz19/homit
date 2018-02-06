47c5ab7afaa1f80ef92e8df3e3e6b4f23db86397
Zaman Zamanli <zzamanli@homit.ca>
2018-02-05 23:26:34 -0700

    Minor changes to sql scripts

--- project_setup/resources/db/dev/2_create_employees.sql
+++ project_setup/resources/db/dev/2_create_employees.sql
@@ -1,7 +1,9 @@
+use homit_dev;
+
 /* Insert employee roles */
-INSERT INTO `homit_dev`.`employee_roles`(id, role) VALUES (2,'CSR');
+INSERT INTO `employee_roles`(id, role) VALUES (2,'CSR');
 
-INSERT INTO `homit_dev`.`users_employees`(user_email, first_name, last_name, password, role_id) VALUES ("csr1@homit.ca", "CSR", "CSR", "$2a$10$ID0LP8Tk7O1bhm/wzNOIdOx9pceiEtTsh/zUUkA.IbOaMT69ZITbC", 2);
+INSERT INTO `users_employees`(user_email, first_name, last_name, password, role_id) VALUES ("csr1@homit.ca", "CSR", "CSR", "$2a$10$ID0LP8Tk7O1bhm/wzNOIdOx9pceiEtTsh/zUUkA.IbOaMT69ZITbC", 2);
 
-INSERT INTO `homit_dev`.`drivers`(id, user_email, first_name, last_name, password) VALUES (2, "driver1@homit.ca", "Chingiz", "Bakhishov", "$2a$10$4BrlOxtvG9GH7IVvUI27OuVFxQkjj6jV1PsGBBEUpM/YjAxOiVYle");
-INSERT INTO `homit_dev`.`drivers`(id, user_email, first_name, last_name, password) VALUES (3, "driver2@homit.ca", "Zaman", "Zamanli", "$2a$10$4BrlOxtvG9GH7IVvUI27OuVFxQkjj6jV1PsGBBEUpM/YjAxOiVYle");
+INSERT INTO `drivers`(id, user_email, first_name, last_name, password) VALUES (2, "driver1@homit.ca", "Chingiz", "Bakhishov", "$2a$10$4BrlOxtvG9GH7IVvUI27OuVFxQkjj6jV1PsGBBEUpM/YjAxOiVYle");
+INSERT INTO `drivers`(id, user_email, first_name, last_name, password) VALUES (3, "driver2@homit.ca", "Zaman", "Zamanli", "$2a$10$4BrlOxtvG9GH7IVvUI27OuVFxQkjj6jV1PsGBBEUpM/YjAxOiVYle");
--- project_setup/resources/db/production/0_create_database.sql
+++ project_setup/resources/db/production/0_create_database.sql
@@ -6,13 +6,10 @@
 * Production password should be different
 */
 DROP DATABASE IF EXISTS homit;
-DROP USER IF EXISTS 'db_web_user'@'localhost';
-DROP USER IF EXISTS 'db_viewer'@'localhost';
+DROP USER IF EXISTS 'homit_web_user'@'localhost';
 
-CREATE USER 'db_web_user'@'localhost' IDENTIFIED BY 'gg:(@SH%H3K5^4#P';
-CREATE USER 'db_viewer'@'localhost' IDENTIFIED BY 'gg:(@SH%H3K5^4#P';
+CREATE USER 'homit_web_user'@'localhost' IDENTIFIED BY 'gg:(@SH%H3K5^4#P';
 
 CREATE DATABASE homit;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON `homit`.* TO 'db_web_user'@'localhost' WITH GRANT OPTION;
-GRANT SELECT ON `homit`.* TO 'db_viewer'@'localhost' WITH GRANT OPTION;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `homit`.* TO 'homit_web_user'@'localhost';
