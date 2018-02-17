6e20465bdee713e7be554250a0c7e708434ecbbd
Jeyhun Gurbanov <jgurbanov@homit.ca>
2018-02-17 22:38:59 +0000

    Merged in coverage (pull request #353)
    HW-13: Expanded coverage map to Rocky View/Tuscany(NW), Panorama(NE), and Riverbend(SE)
    
    Approved-by: Chingiz Bakhishov <chingiz.bakhishov@gmail.com>

--- api_controllers/map.js
+++ api_controllers/map.js
@@ -3,11 +3,6 @@ var router = require("express").Router();
 var current_coverage = [
     // Sount West Border - Old Banff Coach Rd to Signal Hill/Glenmore Trail
 
-    {lat: 51.130962, lng: -114.227161},  // Stoney / Crowchild Trail NW
-    {lat: 51.126101, lng: -114.230909}, // Stoney
-    {lat: 51.120180, lng: -114.231408}, // Stoney
-    {lat: 51.113435, lng: -114.227161}, // Stoney
-    {lat: 51.109199, lng: -114.224162}, // Stoney
     {lat: 51.104806, lng: -114.225411}, // Stoney
     {lat: 51.100335, lng: -114.230159}, // Stoney
     {lat: 51.095784, lng: -114.233407}, // Stoney
@@ -87,16 +82,27 @@ var current_coverage = [
 
     // East Border
 
-    {lat: 50.957193, lng: -114.029548}, // Deerfoot
-    {lat: 50.967221, lng: -114.034118}, // Deerfoot / Southland Dr SE
-    {lat: 50.972612, lng: -114.038839}, // Deerfoot
-    {lat: 50.977203, lng: -114.040568}, // Deerfoot / 11 St SE
-    {lat: 50.982637, lng: -114.037253}, // Deerfoot / Heritage Meadows Rd
-    {lat: 50.987105, lng: -114.035118}, // Deerfoot / Heritage Dr
-    {lat: 50.993142, lng: -114.034989}, // Deerfoot / Glenmore Trail
-    {lat: 50.997315, lng: -114.034024}, // Deerfoot
-    {lat: 51.007591, lng: -114.025977}, // Deerfoot / 15 St SE
-    {lat: 51.010305, lng: -114.010463}, // Deerfoot / Ogden Rd SE
+    {lat: 50.952209, lng: -114.018694}, // Deerfoot East
+    {lat: 50.951453, lng: -114.013783}, // Deerfoot East
+    {lat: 50.949097, lng: -114.004015}, // Deerfoot East / 24 St SE
+    {lat: 50.951717, lng: -114.002787}, // 24 St SE
+    {lat: 50.953545, lng: -114.000862}, // 24 St SE / 114 Ave SE
+    {lat: 50.955233, lng: -114.000555}, // 24 St SE
+    {lat: 50.956529, lng: -114.001213}, // 24 St SE
+    {lat: 50.957566, lng: -114.001548}, // 24 St SE
+    {lat: 50.972680, lng: -114.001639}, // 24 St SE
+    {lat: 50.974673, lng: -114.001348}, // 24 St SE
+    {lat: 50.976459, lng: -113.999269}, // 24 St SE / 86 Ave SE
+    {lat: 50.977220, lng: -113.999009}, // 24 St SE
+    {lat: 50.983194, lng: -113.998768}, // 24 St SE / 78 Ave SE
+    {lat: 50.983249, lng: -113.995182}, // 78 Ave SE
+    {lat: 50.984452, lng: -113.995267}, // 78 Ave SE
+    {lat: 50.985041, lng: -113.995616}, // 78 Ave SE
+    {lat: 50.986998, lng: -113.997339}, // 26a St SE
+    {lat: 50.991278, lng: -114.001687}, // Ogden Rd / 69 Ave SE
+    {lat: 51.001544, lng: -114.011454}, // Ogden Rd
+    {lat: 51.003690, lng: -114.012456}, // Ogden Rd
+    {lat: 51.010248, lng: -114.010524}, // Ogden Rd / Deerfoot SE
     {lat: 51.015084, lng: -114.005056}, // Deerfoot / Peigan Trail SE
     {lat: 51.023345, lng: -114.000786}, // Deerfoot / 34 Ave SE
     {lat: 51.031616, lng: -113.997481}, // Deerfoot
@@ -119,20 +125,20 @@ var current_coverage = [
 
     // North East Border
     
-    {lat: 51.125159, lng: -114.046138}, // Beddington Trail / Deerfoot Trail NW
-    {lat: 51.128039, lng: -114.052893}, // Beddington Trail / Beddington Blvd NW
-    {lat: 51.130170, lng: -114.057360}, // Beddington Trail NW
-    {lat: 51.131316, lng: -114.062431}, // Beddington Trail NW
-    {lat: 51.134301, lng: -114.071270}, // Beddington Trail / Center St NW
-    {lat: 51.136211, lng: -114.075120}, // Beddington Trail NW
-    {lat: 51.138248, lng: -114.081564}, // Beddington Trail / Berkshire Blvd NW
-    {lat: 51.141499, lng: -114.089186}, // Beddington Trail NW
-    {lat: 51.143980, lng: -114.092301}, // Beddington Trail / Country Hills Blvd NW
-    {lat: 51.148682, lng: -114.097248}, // Beddington Trail / Hidden Valley Gate NW
-    {lat: 51.150499, lng: -114.098914}, // Beddington Trail NW
-    {lat: 51.152313, lng: -114.101876}, // Beddington Trail NW
-    {lat: 51.153577, lng: -114.106003}, // Beddington Trail / Hidden Creek Dr NW
-    {lat: 51.155024, lng: -114.109784}, // Beddington Trail NW
+    {lat: 51.125159, lng: -114.046138}, // Beddington Trail / Deerfoot Trail NE
+    {lat: 51.131149, lng: -114.045127}, // Deerfoot Trail NE
+    {lat: 51.141065, lng: -114.035258}, // 96 Ave NE / Deerfoot Trail NE
+    {lat: 51.154393, lng: -114.018252}, // Country Hills Blvd / Deerfoot Trail NE
+    {lat: 51.163326, lng: -114.003915}, // Deerfoot Trail NE
+    {lat: 51.175835, lng: -114.000818}, // Stoney Trail / Deerfoot Trail NE
+    {lat: 51.175670, lng: -114.029168}, // Stoney Trail
+    {lat: 51.175111, lng: -114.045127}, // Stoney Trail
+    {lat: 51.173235, lng: -114.058200}, // Stoney Trail
+    {lat: 51.173597, lng: -114.068700}, // Stoney Trail / Harvest Hills Blvd N
+    {lat: 51.174880, lng: -114.085972}, // Stoney Trail
+    {lat: 51.173564, lng: -114.095527}, // Stoney Trail / 14 St NW
+    {lat: 51.167869, lng: -114.104819}, // Stoney Trail
+    {lat: 51.160330, lng: -114.110384}, // Stoney Trail
 
     // North Border
 
@@ -147,9 +153,32 @@ var current_coverage = [
     {lat: 51.149899, lng: -114.192953}, // Stoney
     {lat: 51.148212, lng: -114.198152}, // Stoney
     {lat: 51.144670, lng: -114.203171}, // Stoney / Country Hill Blvd NW
-    {lat: 51.140566, lng: -114.207420}, // Stoney
-    {lat: 51.136920, lng: -114.216915}, // Stoney
-    {lat: 51.134451, lng: -114.222913}  // Stoney
+    {lat: 51.146717, lng: -114.209347}, // Country Hill Blvd NW
+    {lat: 51.149872, lng: -114.213314}, // Country Hill Blvd NW / 112 Ave NW
+    {lat: 51.152810, lng: -114.216432}, // Country Hill Blvd NW
+    {lat: 51.153622, lng: -114.218075}, // Country Hill Blvd NW
+    {lat: 51.154184, lng: -114.220406}, // Country Hill Blvd NW
+    {lat: 51.154118, lng: -114.234274}, // Country Hill Blvd NW / Rocky Ridge Road NW
+    {lat: 51.154122, lng: -114.241365}, // Country Hill Blvd NW
+    {lat: 51.154025, lng: -114.242352}, // Country Hill Blvd NW
+    {lat: 51.154016, lng: -114.248485}, // Country Hill Blvd NW / Rocky Ridge Blvd NW
+    {lat: 51.154008, lng: -114.252855}, // Country Hill Blvd NW / Rockyspring Gate NW
+    {lat: 51.153796, lng: -114.254971}, // Country Hill Blvd NW / Rockyspring Gate NW
+    {lat: 51.153967, lng: -114.257478}, // Country Hill Blvd NW / 12 Mile Coulee Rd NW
+    {lat: 51.145680, lng: -114.257428}, // 12 Mile Coulee Rd NW
+    {lat: 51.144198, lng: -114.256611}, // 12 Mile Coulee Rd NW
+    {lat: 51.143374, lng: -114.256990}, // 12 Mile Coulee Rd NW / Crowchild Rd
+    {lat: 51.142179, lng: -114.257499}, // 12 Mile Coulee Rd NW
+    {lat: 51.132390, lng: -114.257426}, // 12 Mile Coulee Rd NW / Tuscany Way NW
+    {lat: 51.117679, lng: -114.257461}, // 12 Mile Coulee Rd NW
+    {lat: 51.107596, lng: -114.256948}, // Two Toed Pond
+    {lat: 51.107139, lng: -114.243856}, // Nose Hill Dr NW
+    {lat: 51.106575, lng: -114.224708}, // Nose Hill Dr NW / Stoney Trail NW
+    
+
+    // {lat: 51.140566, lng: -114.207420}, // Stoney
+    // {lat: 51.136920, lng: -114.216915}, // Stoney
+    // {lat: 51.134451, lng: -114.222913}  // Stoney
 ]
 
 router.get("/coverage", function(req, res, next){
