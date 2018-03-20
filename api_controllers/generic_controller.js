bdb3872f7109a3a0da4ed47846b5bc7911a38d77
Zaman Zamanli <zzamanli@homit.ca>
2018-03-20 03:05:19 +0000

    Merged in minor-bug (pull request #433)
    Remove driver from api controllers
    
    Approved-by: Chingiz Bakhishov <chingiz.bakhishov@gmail.com>

--- api_controllers/generic_controller.js
+++ api_controllers/generic_controller.js
@@ -7,7 +7,6 @@ router.use("/myaccount", require(path.join(__dirname, "./myaccount")));
 router.use("/cart", require(path.join(__dirname, "./cart")));
 router.use("/checkout", require(path.join(__dirname, "./checkout")));
 router.use("/map", require(path.join(__dirname, "./map")));
-router.use("/driver", require(path.join(__dirname, "./driver")));
 router.use("/sms", require(path.join(__dirname, "./sms")));
 router.use("/csr", require(path.join(__dirname, "./csr")));
 
