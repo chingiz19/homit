889f0acb3dc1d926fccc7d032a1d713ef1f2b4bb
Jeyhun Gurbanov <jgurbanov@homit.ca>
2018-05-05 01:20:11 +0000

    Merged in helpers (pull request #518)
    Web 28 - Adding helper_utils to F.E
    
    Approved-by: Chingiz Bakhishov <chingiz.bakhishov@gmail.com>

---
+++ public/resources/services/helpers.js
@@ -0,0 +1,13 @@
+/**
+ * This service is for helper functions that are commonly used in many placesd
+ * 
+ * 
+ * @copyright Homit
+ * @author Jeyhun Gurbanov
+ */
+app.service('helpers', function(){
+    var pub = {};
+
+
+    return pub;
+});
